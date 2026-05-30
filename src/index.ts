332d8ddee96dec5e0906556e4f811b035eaa40b5
PULKIT <pulkit.329866@gmail.com>
2026-05-30 15:02:26 +0530

    Prisma setup Done

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
+import { prismaClient } from './lib/db';
 
 async function init() {
     const app = express();
@@ -15,19 +16,32 @@ async function init() {
                 hello: String
                 say(name: String): String
             }
+            type Mutation {
+                createUser(firstName: String!, lastName: String!, email: String!, password: String!): Boolean
+            }
         
         `,
         resolvers: {
             Query: {
                 hello: () => "Hey there! I am GraphQL Server",
-                say: (_, {name}: {name: String} ) => `Hey ${name}, How are you buddy?`
+                say: (_, { name }: { name: String }) => `Hey ${name}, How are you buddy?`
             },
-        },
+            Mutation: {
+                createUser: async (_, { firstName, lastName, email, password }: { firstName: string, lastName: string, email: string, password: string }) => {
+                    await prismaClient.user.create({
+                        data: {
+                            firstName, lastName, email, password, salt: "Aditi Goyal"
+                        },
+                    });
+                    return true;
+                }
+            }
+        }
     })
 
     // Start the gqlServer 
     await gqlServer.start();
-    
+
     app.get("/", (req, res) => {
         res.json({ message: "Server is Up and Running" });
     });
